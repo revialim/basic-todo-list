728ab29c52b81e946b11284e88773d8b85c402d4
Lilian Hung <l.hung.m@gmail.com>
2022-05-19 08:30:16 +0200

    Add priority and type for editing todo

--- components/todo-element.tsx
+++ components/todo-element.tsx
@@ -16,6 +16,9 @@ export const TodoElement: React.FunctionComponent<TodoElementProps> = props => {
   const [editMode, setEditMode] = useState(false);
   const [todoTitleEdit, setTodoTitleEdit] = useState(todo.title);
   const [todoDescEdit, setTodoDescEdit] = useState(todo.description);
+  const [todoPriorityEdit, setTodoPriorityEdit] = useState(todo.priority);
+  const [todoTypeEdit, setTodoTypeEdit] = useState(todo.type);
+  // const [todoIsDone, setTodoIsDone] = useState(todo.isDone);
 
   const handleDelete = async (e:any) => {
     e.preventDefault();
@@ -33,8 +36,8 @@ export const TodoElement: React.FunctionComponent<TodoElementProps> = props => {
       {
         title: todoTitleEdit,
         description: todoDescEdit,
-        priority: 9,
-        type:1,
+        priority: todoPriorityEdit,
+        type: todoTypeEdit,
       }
     ).then((res) => {
       // console.log('todoElement => handleSubmit => res', res);
@@ -44,6 +47,8 @@ export const TodoElement: React.FunctionComponent<TodoElementProps> = props => {
   };
   const handleTitleChange = (e: any) => {setTodoTitleEdit(e.target.value)};
   const handleDescChange = (e: any) => {setTodoDescEdit(e.target.value)};
+  const handlePriorityChange = (e: any) => {setTodoPriorityEdit(e.target.value)};
+  const handleTypeChange = (e: any) => {setTodoTypeEdit(e.target.value)};
   
   if(!editMode){ // VIEW MODE
     return (
@@ -77,6 +82,18 @@ export const TodoElement: React.FunctionComponent<TodoElementProps> = props => {
           <div className="input-group input-group-sm mb-3">
               <span className="input-group-text" >Description</span>
               <textarea defaultValue={todoDescEdit} onChange={handleDescChange} className="form-control" aria-label="With textarea"></textarea>
+          </div>
+          <div>
+            <label htmlFor="customRange2" className="form-label">Priority: {todoPriorityEdit}</label>
+            <input type="range" className="form-range" defaultValue={todoPriorityEdit} min="1" max="10" id="customRange2" onChange={handlePriorityChange}/>
+          </div>
+          <div>
+            <p>Type: {todoTypeEdit}</p>
+            <select className="form-select form-select-sm" aria-label=".form-select-sm example" onChange={handleTypeChange}>
+              <option value="1">work</option>
+              <option value="2">personal</option>
+              <option value="3">important</option>
+            </select>
           </div>
           <button type="button" className="btn btn-secondary" onClick={e => setEditMode(false)}>Cancel</button>
           <button type="button" className="btn btn-primary" onClick={handleSubmit}>Save</button>
